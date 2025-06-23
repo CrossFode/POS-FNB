d08845ac69fa6d03abd2862b5ff56f517d08772d
Aldinorasendriya <95845758+Aldinorasend@users.noreply.github.com>
2025-06-24 01:27:42 +0700

    feat(login) : save state after login

--- posmobile/lib/Pages/Dashboard/HomeSuperAdmin.dart
+++ posmobile/lib/Pages/Dashboard/HomeSuperAdmin.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:posmobile/Auth/login.dart';
 // import 'package:posmobile/Components/NavbarSuperAdmin.dart';
 import 'package:posmobile/Pages/Pages.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../Model/Model.dart';
 
@@ -21,7 +23,8 @@ class HomePageSuperAdmin extends StatefulWidget {
 class _HomePageSuperAdminState extends State<HomePageSuperAdmin> {
   int _currentIndex = 0;
   late Future<OutletResponse> _outletFuture;
-
+  String? token = "";
+  String? role = "";
   late final List<Widget> _pages;
   final String baseUrl = dotenv.env['API_BASE_URL'] ?? '';
   // final String baseUrl = 'https://pos.lakesidefnb.group';
@@ -54,115 +57,148 @@ class _HomePageSuperAdminState extends State<HomePageSuperAdmin> {
       // StatisticsPage(token: widget.token, outletId: widget.outletId),
       // You can add OutletPage to _pages if needed, but not as a Future
     ];
+    getCred();
+  }
+
+  void getCred() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      token = pref.getString("token");
+      role = pref.getString('role');
+      print("Token dari SharedPreferences: $token");
+      print("Role dari SharedPreferences: $role");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Super Admin Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: SafeArea(
+      child: Column(children: [
+        Row(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Center(
+            TextButton(
+                onPressed: () async {
+                  SharedPreferences pref =
+                      await SharedPreferences.getInstance();
+                  pref.remove('token');
+
+                  pref.remove('role');
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => LoginPage()));
+                  print("Token dihapus");
+                  print("Role dihapus");
+                },
                 child: Text(
-                  'Main Menu',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins',
+                  "Logout",
+                  style: TextStyle(color: Colors.red, fontSize: 16),
+                ))
+          ],
+        ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Center(
+                  child: Text(
+                    'Main Menu',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Poppins',
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 300,
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                shrinkWrap: true,
-                children: [
-                  // Tombol ke OutletPage
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => OutletPage(
-                            token: widget.token,
-                            outletId: '', // Isi sesuai kebutuhan
+              SizedBox(
+                height: 300,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  shrinkWrap: true,
+                  children: [
+                    // Tombol ke OutletPage
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => OutletPage(
+                              token: widget.token,
+                              outletId: '', // Isi sesuai kebutuhan
+                            ),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.store, size: 40),
+                              SizedBox(height: 8),
+                              Text(
+                                'Outlet',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w600),
+                              ),
+                            ],
                           ),
                         ),
-                      );
-                    },
-                    child: Card(
-                      elevation: 6,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
                       ),
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.store, size: 40),
-                            SizedBox(height: 8),
-                            Text(
-                              'Outlet',
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w600),
-                            ),
-                          ],
+                    ),
+                    // Tombol ke UserPage
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UserPage(
+                                    token: widget.token,
+                                    outletId: '',
+                                  )),
+                        );
+                      },
+                      child: Card(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.person, size: 40),
+                              SizedBox(height: 8),
+                              Text(
+                                'User',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  // Tombol ke UserPage
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => UserPage(
-                                  token: widget.token,
-                                  outletId: '',
-                                )),
-                      );
-                    },
-                    child: Card(
-                      elevation: 6,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.person, size: 40),
-                            SizedBox(height: 8),
-                            Text(
-                              'User',
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w600),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  // Tambahkan tombol menu lain di sini sesuai kebutuhan
-                ],
+                    // Tambahkan tombol menu lain di sini sesuai kebutuhan
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      ]),
+    ));
   }
 }
